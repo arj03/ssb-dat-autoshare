1b9093405deadf26da6cab9aefb5cdf7ec500421
Anders Rune Jensen <arj03@protonmail.ch>
2017-09-19 14:37:43 +0200

    Fix indention

--- index.js
+++ index.js
@@ -10,39 +10,39 @@ program
   .parse(process.argv);
 
 require('ssb-client')((err, sbot) => {
-    if (err) throw err;
+  if (err) throw err;
 
-    console.log("Looking for dat links")
-    
-    pull(
-	sbot.createLogStream({ reverse: true, limit: 10000 }),
-	pull.filter((msg) => {
-	    return !msg.value ||
-		msg.value.content.type == 'post' &&
-		typeof msg.value.content.text == "string" &&
-		msg.value.content.text.indexOf("dat://") != -1
-	}),
-	pull.collect((err, log) => {
-            if (err) throw err;
+  console.log("Looking for dat links")
 
-	    console.log("Found " + log.length)
+  pull(
+    sbot.createLogStream({ reverse: true, limit: 10000 }),
+    pull.filter((msg) => {
+      return !msg.value ||
+	msg.value.content.type == 'post' &&
+	typeof msg.value.content.text == "string" &&
+	msg.value.content.text.indexOf("dat://") != -1
+    }),
+    pull.collect((err, log) => {
+      if (err) throw err;
+
+      console.log("Found " + log.length)
+
+      log.forEach(msg => {
+	uri.withinString(msg.value.content.text, (datLink) => {
+	  if (!datLink.startsWith("dat://")) return
+
+	  console.log("Saving to:", program.folder + "/" + datLink.substring(6))
+
+	  Dat(program.folder + "/" + datLink.substring(6), {
+	    key: datLink
+	  }, function (err, dat) {
+	    if (err) throw err
 	    
-            log.forEach(msg => {
-		uri.withinString(msg.value.content.text, (datLink) => {
-		    if (!datLink.startsWith("dat://")) return
-
-		    console.log("Saving to:", program.folder + "/" + datLink.substring(6))
-		    
-		    Dat(program.folder + "/" + datLink.substring(6), {
-			key: datLink
-		    }, function (err, dat) {
-			if (err) throw err
-			
-			console.log("sharing:", datLink)
-			dat.joinNetwork()
-		    })
-		})
-	    })
+	    console.log("sharing:", datLink)
+	    dat.joinNetwork()
+	  })
 	})
-    )
+      })
+    })
+  )
 })
